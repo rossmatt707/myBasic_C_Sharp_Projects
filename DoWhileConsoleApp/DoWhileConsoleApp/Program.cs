3de7b7dea63753360dc1a1d8ab4253572dbd042b
rossmatt707 <rossmat707@gmail.com>
2024-10-10 15:37:09 -0700

    added Do/Do while loop program

---
+++ DoWhileConsoleApp/DoWhileConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics.Eventing.Reader;
+
+namespace DoWhileConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While loop
+            int count = 0;
+            // Loop continues while count is less than 5
+            while (count < 10)
+            {
+                // Writes count value to console
+                Console.WriteLine("Count: " + count);
+                // Increments count by 1
+                count++;
+                // Pauses for 500 milliseconds
+                Thread.Sleep(500);
+            }
+            if (count == 10)
+            {
+                // Writes "Happy new year!" to console when count is 10
+                Console.WriteLine("Happy new year!");
+            }
+            // Resets count to 0
+            count = 0;
+            // Line break
+            Console.WriteLine("\n");
+            // Do-While loop
+            do
+            {
+                // Writes count value to console
+                Console.WriteLine("Count: " + count);
+                // Increments count by 1
+                count++;
+                // Pauses for 500 milliseconds
+                Thread.Sleep(500);
+            }
+            // Loop continues while count is less than 5
+            while (count < 10);
+            if (count == 10) 
+            {
+                // Writes "Happy new year!" to console when count is 10
+                Console.WriteLine("Happy new year!");
+            }
+        }
+    }
+}
---
+++ LoopsVideo/LoopsVideo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoopsVideo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoopsVideo")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71e94ea9-5759-4aff-ac23-a53796071fa8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
