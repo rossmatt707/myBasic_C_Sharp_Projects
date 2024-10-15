5cbc6455dc44b009d326e4c5e4456c3f1a13be58
rossmatt707 <rossmat707@gmail.com>
2024-10-15 13:58:48 -0700

    ClassSubmission4 added

---
+++ ClassSubmission4/ClassSubmission4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClassSubmission4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the VoidClass
+            VoidClass voidClass = new VoidClass();
+
+            Console.WriteLine("Please enter a number: "); // Prompting the user to enter a number
+            int num1 = Convert.ToInt32(Console.ReadLine()); // Converting the user input to an integer
+
+            int result; // Declaring the result variable
+            voidClass.DivideByTwo(num1, out result); // Calling the DivideByTwo method and passing arguments
+            Console.WriteLine($"Your number divided by two is {result}!"); // Displaying the result
+            Thread.Sleep(1000); // Pausing the program for 1 second
+
+            StaticClass.GetOutput(out int number, out string message); // Calling the GetOutput method with two out parameters
+            Console.WriteLine($"\nThe number is {number} and the message is {message}."); // Displaying the output
+            Thread.Sleep(1000); // Pausing the program for 1 second
+
+            Console.WriteLine("\nPlease enter a message: "); // Prompting the user to enter a message
+            string input = Console.ReadLine(); // Storing the user input in a variable
+            StaticClass.GetOutput(input, out string message2); // Calling the GetOutput method with a string input and an out parameter
+            Console.WriteLine($"{message2} {input}"); // Displaying the output
+            Console.ReadLine(); // Keeping the console window open
+        }
+    }   
+}
---
+++ ClassSubmission4/ClassSubmission4/VoidClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission4
+{
+    // Internal class with a void method
+    internal class VoidClass
+    {
+        // Public Void method that takes an integer and divides it by two
+        public void DivideByTwo(int num1, out int result)
+        {
+            result = num1 / 2;
+        }
+    }
+
+    // Static class with two overloaded methods
+    internal static class StaticClass
+    {
+        // Static method that takes two out parameters
+        public static void GetOutput(out int number, out string message)
+        {
+            number = 777;
+            message = "Hello!";
+        }
+
+        // Static method that takes a string input and an out parameter
+        public static void GetOutput(string input, out string message2)
+        {
+            // Concatenating the input with a message
+            message2 = $"This is what you wanted to tell the people:";
+        }
+    }
+    
+}
