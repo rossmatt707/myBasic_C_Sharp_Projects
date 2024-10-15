f65d605e72f8ad2b5e26dd99cda18aa48bd9512b
rossmatt707 <rossmat707@gmail.com>
2024-10-15 09:37:23 -0700

    MethodSubmission3Void assignment added

---
+++ MethodSubmission3Void/MethodSubmission3Void/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission3Void
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the VoidClass
+            var voidClass = new VoidClass();
+            // Call the VoidMethod and pass in two integers by name
+            voidClass.VoidMethod(num1: 25, numVoid: 13245);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ MethodSubmission3Void/MethodSubmission3Void/VoidClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MethodSubmission3Void
+{
+    internal class VoidClass
+    {
+        // Void method that takes two integers as parameters
+        public void VoidMethod(int num1, int numVoid)
+        {
+            // Multiply the first integer by 4
+            num1 = num1 * 4;
+            // Display the result of the multiplication
+            Console.WriteLine($"Your first number multiplied by four is {num1}!");
+            Thread.Sleep(1000);
+            // Display the second integer
+            Console.WriteLine($"Your second void number.. don't worry we left it alone: {numVoid}");
+        }
+    }
+}
