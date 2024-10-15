defc40805990db623b7d7c6396b9c25b4ce801ab
rossmatt707 <rossmat707@gmail.com>
2024-10-15 12:00:13 -0700

    MethodSubmission3Void satisfied step #3

--- MethodSubmission3Void/MethodSubmission3Void/Program.cs
+++ MethodSubmission3Void/MethodSubmission3Void/Program.cs
@@ -12,6 +12,8 @@ namespace MethodSubmission3Void
         {
             // Instantiate the VoidClass
             var voidClass = new VoidClass();
+            // Call the VoidMethod and pass in two integers using position
+            voidClass.VoidMethod(25, 13245);
             // Call the VoidMethod and pass in two integers by name
             voidClass.VoidMethod(num1: 25, numVoid: 13245);
             Console.ReadLine();
